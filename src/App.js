5b6fee657373112ba05cadea1cee39979a6b5458
jzinit <kyj32323@naver.com>
2024-06-14 09:01:31 +0900

    # 동적 리스트 렌더링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './App.css';
 import ExpenseList from './components/expenses/ExpenseList';
-import Greet from './components/Greet';
-// import Counter from './components/practice/Counter';
 import NewExpense from './components/new-expense/NewExpense';
-import CheckBoxStyle from './components/practice/CheckBoxStyle';
 
 const App = () => {
 
@@ -13,31 +10,66 @@ const App = () => {
     {
       title: '치킨먹음',
       price: 30000,
-      date: new Date(2024, 6 - 1, 3)
+      date: new Date(2023, 6 - 1, 3)
     },
     {
       title: '족발먹음',
       price: 40000,
-      date: new Date(2024, 6 - 1, 7)
+      date: new Date(2022, 2 - 1, 7)
     },
     {
       title: '헬스장등록',
       price: 300000,
-      date: new Date(2024, 6 - 1, 12)
+      date: new Date(2021, 6 - 1, 12)
+    },
+    {
+      title: '피ㅑㅈ',
+      price: 40000,
+      date: new Date(2020, 4 - 1, 9)
+    },
+    {
+      title: '물회 ',
+      price: 78000,
+      date: new Date(2019, 9 - 1, 7)
+    },
+    {
+      title: '오징어회 ',
+      price: 78000,
+      date: new Date(2024, 9 - 1, 7)
+    },
+    {
+      title: '우럭 ',
+      price: 78000,
+      date: new Date(2022, 9 - 1, 7)
+    },
+    {
+      title: '광어 ',
+      price: 78000,
+      date: new Date(2019, 9 - 1, 7)
+    },
+    {
+      title: '매운탕 ',
+      price: 78000,
+      date: new Date(2019, 9 - 1, 7)
+    },
+    {
+      title: '회덮밥 ',
+      price: 78000,
+      date: new Date(2024, 9 - 1, 7)
     },
   ];
 
-  //ExpenseForm 에게 내려보낼 함수
+
+  // ExpenseForm에게 내려보낼 함수
   const onAddExpense = (userInput) => {
-    console.log('app.js가 내려보낸 함수 호출 !');
-    console.log(userInput);
+    console.log('App.js 가 내려보낸 함수 호출!');
+    // console.log(userInput);
     expenses.push(userInput);
     console.log(expenses);
   };
 
   return (
     <>
-      {/* <CheckBoxStyle /> */}
       <NewExpense onSave={onAddExpense} />
       <ExpenseList expenses={expenses} />
     </>
