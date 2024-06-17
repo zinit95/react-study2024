d1904a8394ef1436a11c583721bd7e6c98ff58ea
jzinit <kyj32323@naver.com>
2024-06-17 09:19:31 +0900

    동적 인라인 스타일 변경

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -2,20 +2,42 @@ import React, { useState } from 'react';
 import './CourseInput.css';
 import Button from '../UI/Button';
 
-const CourseInput = ({onAdd}) => {
+const CourseInput = ({ onAdd }) => {
+
+  // 목표 인풋에 입력한 값
   const [enteredText, setEnteredText] = useState('');
+
+  // 입력값 검증을 통과했는지 여부를 상태관리
+  const [isValid, setIsValid] = useState(false);
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
+
+    if (enteredText.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     const newGoalObject = {
       id: Math.random().toString(),
       text: enteredText,
     };
-    //console.log(newGoalObject);
+    // console.log(newGoalObject);
 
     onAdd(newGoalObject);
 
     setEnteredText('');
+  };
+
+  const goalChangeHandler = (e) => {
+
+    const inputValue = e.target.value;
+
+    // 입력값 검증
+    if (inputValue.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredText(inputValue);
   };
 
   return (
@@ -24,8 +46,12 @@ const CourseInput = ({onAdd}) => {
         <label>나의 목표</label>
         <input
           type="text"
-          onChange={(e) => setEnteredText(e.target.value)}
+          onChange={goalChangeHandler}
           value={enteredText}
+          style={{
+            backgroundColor : isValid ? 'transParent' : 'salmon',
+            borderColor : isValid ? 'blue' : 'red',
+          }}
         />
       </div>
       <Button type="submit">목표 추가하기</Button>
