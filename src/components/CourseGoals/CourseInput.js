b8480702f526b105fd79c4199ca7970978cc3743
jzinit <kyj32323@naver.com>
2024-06-17 10:40:21 +0900

    # fragment

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 import Button from '../UI/Button';
 
 const CourseInput = ({ onAdd }) => {
+
+  // console.log('s', styles);
+  
+  const {invalid, 'form-control' : formControl} = styles;
 
   // 목표 인풋에 입력한 값
   const [enteredText, setEnteredText] = useState('');
@@ -40,18 +44,37 @@ const CourseInput = ({ onAdd }) => {
     setEnteredText(inputValue);
   };
 
+
+  const foo = () => {
+
+    const arr = [1,2,3,4,5,6,700000000];
+
+    // 렌더링이 된 요소
+    const $ul = document.querySelector('ul.abc');
+
+    // 중간 가상 DOM 생성
+    const $div = document.createDocumentFragment();
+
+    arr.forEach(n => {
+      // 아직 렌더링이 되지 않은 요소 : virtual DOM
+      const $li = document.createElement('li');
+      $li.textContent = n;
+      // 렌더링된 곳에 렌더링되지않은 요소를 추가 : 성능상 좋지 않다.
+      $div.appendChild($li);
+    });
+
+    $ul.appendChild($div);
+  };
+
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`${formControl} ${isValid ? 'invalid' : ''}`}>
         <label>나의 목표</label>
         <input
           type="text"
           onChange={goalChangeHandler}
           value={enteredText}
-          style={{
-            backgroundColor : isValid ? 'transParent' : 'salmon',
-            borderColor : isValid ? 'blue' : 'red',
-          }}
         />
       </div>
       <Button type="submit">목표 추가하기</Button>
