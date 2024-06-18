928e35c806be6835da9b0049fd653850e8afea57
jzinit <kyj32323@naver.com>
2024-06-18 09:56:49 +0900

    # useEffect 기초

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import MainHeader from './components/SideEffect/MainHeader';
 import Home from './components/SideEffect/Home';
@@ -9,16 +9,28 @@ const App = () => {
 
   // 현재 로그인 상태를 체크하는 변수
   const [isLoggedIn, setIsloggedIn] = useState(false);
-  console.log('로그인 검사 수행!');
 
-  //localStorage 에서 login-flag 를 꺼냄
-  const storedLoginFlag = localStorage.getItem('login-flag');
-  // 로그인 검사를 초기에 수행 
-  if(storedLoginFlag == 1){
-    //상태 변수가 setter 로 변경되면
-    //리액트는 변경감지 후 바로 리렌더링을 수행함 
-    setIsloggedIn(true);
-  }
+  // console.log('로그인 검사 수행!');
+  // localStorage에서 login-flag를 꺼냄
+  // const storedLoginFlag = localStorage.getItem('login-flag');
+  // 로그인 검사를 초기에 1번만 수행
+  // if (storedLoginFlag === '1') {
+    // 상태변수가 setter로 변경되면 
+    // 리액트는 변경감지 후 바로 리렌더링을 수행함
+    // setIsloggedIn(true);
+  // }
+
+
+  // side effect 처리를 위한 함수  
+  //useEffect 는 기본적으로 컴포넌트 렌더링시 단 한번만 호출, 새로고침해도 무한루프가 돌지 않는다 
+  useEffect(() => {
+    console.log('로그인 검사 수행!');
+    const storedLoginFlag = localStorage.getItem('login-flag');
+    if (storedLoginFlag === '1') {
+      setIsloggedIn(true);
+    }
+  }, []);
+
 
   // 서버 통신은 중앙집중 관리가 중요함
   const loginHandler = (email, password) => {
@@ -28,9 +40,14 @@ const App = () => {
     setIsloggedIn(true);
   };
 
+  const logoutHandler = () => {
+    localStorage.removeItem('login-flag');
+    setIsloggedIn(false);
+  };
+
   return (
     <>
-      <MainHeader />
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {isLoggedIn && <Home />}
         {!isLoggedIn && <Login onLogin={loginHandler} />}
@@ -39,6 +56,5 @@ const App = () => {
   );
 };
 // {isLoggedIn && <Home />} //isLoggedIn이 true 면 <Home /> 보여줘
-// {!isLoggedIn && <Login onLogin={loginHandler} />} //isLoggedIn이 false 면 onLogin={loginHandler} 보여줘
-
+// {!isLoggedIn && <Login onLogin={loginHandler} />} //isLoggedIn이 false 면 
 export default App;
