b0d2c10c2f1e8e8eb3be273d10f9de9e363f35c4
jzinit <kyj32323@naver.com>
2024-06-18 09:49:06 +0900

    # 로그인시 발행하는 side effect

--- src/App.js
+++ src/App.js
@@ -7,15 +7,38 @@ import Login from './components/SideEffect/Login';
 
 const App = () => {
 
+  // 현재 로그인 상태를 체크하는 변수
+  const [isLoggedIn, setIsloggedIn] = useState(false);
+  console.log('로그인 검사 수행!');
+
+  //localStorage 에서 login-flag 를 꺼냄
+  const storedLoginFlag = localStorage.getItem('login-flag');
+  // 로그인 검사를 초기에 수행 
+  if(storedLoginFlag == 1){
+    //상태 변수가 setter 로 변경되면
+    //리액트는 변경감지 후 바로 리렌더링을 수행함 
+    setIsloggedIn(true);
+  }
+
+  // 서버 통신은 중앙집중 관리가 중요함
+  const loginHandler = (email, password) => {
+
+    // 로그인의 증거로 클라이언트에 1이라는 숫자를 기록
+    localStorage.setItem('login-flag', '1');
+    setIsloggedIn(true);
+  };
+
   return (
     <>
       <MainHeader />
       <main>
-        {/* <Home /> */}
-        <Login />
+        {isLoggedIn && <Home />}
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
       </main>
     </>
   );
 };
+// {isLoggedIn && <Home />} //isLoggedIn이 true 면 <Home /> 보여줘
+// {!isLoggedIn && <Login onLogin={loginHandler} />} //isLoggedIn이 false 면 onLogin={loginHandler} 보여줘
 
 export default App;
