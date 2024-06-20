5659a258e8ba600f2c633b66aed0b33d69b3a1cd
jzinit <kyj32323@naver.com>
2024-06-20 10:47:15 +0900

    # 도전과제 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Player from './components/Ref/Player';
+import TimerChallenge from './components/Ref/TimeChallenge';
 
 const App = () => {
 
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title='Easy' targetTime={1} />
+        <TimerChallenge title='Not Easy' targetTime={5} />
+        <TimerChallenge title='Getting tough' targetTime={10} />
+        <TimerChallenge title='Pros only' targetTime={15} />
+      </div>
     </>
   );
 };
--- src/components/Ref/Player.js
+++ src/components/Ref/Player.js
@@ -1,18 +1,18 @@
 import React, { useRef, useState } from "react";
 
 const Player = () => {
-  
+
   const $nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('anonymous');
 
-  const nameChangeHandler = e =>{
+  const nameChangeHandler = e => {
     setEnteredName($nameInputRef.current.value);
     $nameInputRef.current.value = '';
   };
 
   return (
     <section id="player">
-      <h2>Welcome {enteredName}</h2>
+      <h2>Welcome {enteredName}!</h2>
       <p>
         <input ref={$nameInputRef} type="text"/>
         <button onClick={nameChangeHandler}>Set Name</button>
