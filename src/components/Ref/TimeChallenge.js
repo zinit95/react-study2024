48014444454aa47d8f35c674cb40e3d9e4d05a42
jzinit <kyj32323@naver.com>
2024-06-20 12:31:16 +0900

    점수 출력

--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -5,44 +5,55 @@ const TimerChallenge = ({ title, targetTime }) => {
   // timer를 ref변수로 관리
   const timer = useRef();
 
-  // 타이머가 시작되었는지를 확인하는 상태값
-  const [timerStarted, setTimerStarted] = useState(false);
-
-  // 타겟시간이 종료되었는지 여부
-  const [timerExpired, setTimerExpired] = useState(false);
-
   // 자식컴포넌트 ResultModal에 있는 dialog태그의 참조를 만듬
   const dialog = useRef();
 
-  const startHandler = (e) => {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      // modal open!
-      dialog.current.showModal();
-    }, targetTime * 1000);
+  // stop을 눌렀을 때 남은시간을 상태값으로 관리
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
-    setTimerStarted(true);
+  // start stop 활성화 조건
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  // 시간이 타임오버되었을 때 처리
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.showModal();
+  }
+
+  const startHandler = (e) => {
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTime) => prevTime - 10);
+    }, 10);
   };
 
   const stopHandler = (e) => {
-    clearTimeout(timer.current);
+    clearInterval(timer.current);
+    dialog.current.showModal();
   };
+
+  //남은시간 리셋 함수
+  const resetHandler = () => setTimeRemaining(targetTime * 1000);
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset= {resetHandler}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
         <p>
-          <button onClick={timerStarted ? stopHandler : startHandler}>
-            {timerStarted ? 'Stop' : 'Start'} Challenge
+          <button onClick={timerIsActive ? stopHandler : startHandler}>
+            {timerIsActive ? 'Stop' : 'Start'} Challenge
           </button>
         </p>
-        <p className={timerStarted ? 'active' : undefined}>
-          {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        <p className={timerIsActive ? 'active' : undefined}>
+          {timerIsActive ? 'Time is running...' : 'Timer inactive'}
         </p>
       </section>
     </>
