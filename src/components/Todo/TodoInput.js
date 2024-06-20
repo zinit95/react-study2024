d3f03a9e7d867ef69444d44d864dab1772610f07
jzinit <kyj32323@naver.com>
2024-06-20 09:12:05 +0900

    # todo 조건부 렌더링

--- src/components/Todo/TodoInput.js
+++ src/components/Todo/TodoInput.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import {MdAdd} from "react-icons/md";
+import React, { useState } from 'react';
+import { MdAdd } from 'react-icons/md';
 
 import './scss/TodoInput.scss';
 
 const TodoInput = () => {
+  // 입력창 토글링 상태값
+  const [open, setOpen] = useState(false);
+
+  // 버튼 토글링 함수
+  const onToggle = () => setOpen(prevOpen => !prevOpen);
+
   return (
     <>
-      <div className='form-wrapper'>
-        <form className='insert-form'>
-          <input
-            type='text'
-            placeholder='할 일을 입력 후, 엔터를 누르세요!'
-          />
-        </form>
-      </div>
-      <button className='insert-btn'>
-        <MdAdd/>
+      {open && (
+        <div className="form-wrapper">
+          <form className="insert-form">
+            <input
+              type="text"
+              placeholder="할 일을 입력 후, 엔터를 누르세요!"
+            />
+          </form>
+        </div>
+      )}
+
+      <button
+        className={`insert-btn ${open ? 'open' : undefined}`}
+        onClick={onToggle}
+      >
+        <MdAdd />
       </button>
     </>
   );
