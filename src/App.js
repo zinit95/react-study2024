13eb078ad6243799ab99a232d23baa88ad34d2cc
jzinit <kyj32323@naver.com>
2024-06-24 08:57:38 +0900

    # 장바구니 리듀서에 추가액션 제어

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import Player from './components/Ref/Player';
-import TimerChallenge from './components/Ref/TimeChallenge';
+import React, { useState } from 'react';
+import Header from './components/Food/Layout/Header';
+import Meals from './components/Food/Meals/Meals';
+import Cart from './components/Food/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 const App = () => {
 
+  // 장바구니 모달을 열고 닫는 상태변수
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  // 모달을 열어주는 핸들러
+  const showCartHandler = () => setCartIsShown(true);
+
+  // 모달을 닫아주는 핸들러
+  const hideCartHandler = () => setCartIsShown(false);
+
   return (
-    <>
-      <Player />
-      <div id="challenges">
-        <TimerChallenge title='Easy' targetTime={1} />
-        <TimerChallenge title='Not Easy' targetTime={5} />
-        <TimerChallenge title='Getting tough' targetTime={10} />
-        <TimerChallenge title='Pros only' targetTime={15} />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <div id="main">
+        <Meals />
       </div>
-    </>
+    </CartProvider>
   );
 };
 
