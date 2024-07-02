ad1dcbb7c1dd6e38bc87c19624958e962b4f6648
jzinit <kyj32323@naver.com>
2024-07-03 08:54:48 +0900

    # 동적 라우팅

--- src/components/Food/Layout/HeaderCartButton.js
+++ src/components/Food/Layout/HeaderCartButton.js
@@ -1,23 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from './CartIcon';
 import styles from './HeaderCartButton.module.scss';
 import CartContext from '../../../store/cart-context';
 
 const HeaderCartButton = ({ onShow }) => {
 
+  // bump 애니메이션을 제어하는 상태변수
+  const [isBump, setIsBump] = useState(false);
+
+  // 장바구니 배열
   const { cartItems } = useContext(CartContext);
 
-  // 헤더에 총 수량 표시 
-  const numberOfCart = cartItems.reduce((accum, current) => { //accum : 누적자 
-    //console.log('accum: ', accum);
-    //console.log('current: ', current);
-    return accum + current.amount;
-  }, 0);
+  const numberOfCart = cartItems.reduce((accum, current) => accum + current.amount, 0);
 
-  const {button, icon, badge} = styles;
+  const {button, icon, badge, bump} = styles;
+
+  useEffect(() => {
+    if (cartItems.length === 0) return;
+
+    // console.log('useEffect 실행');
+    setIsBump(true);
+
+    // 애니메이션 실행 후 클래스 제거
+    const timer = setTimeout(() => {
+      setIsBump(false);
+    }, 300);
+
+    return () => clearTimeout(timer);
+    
+  }, [cartItems]);
 
   return (
-    <button className={button} onClick={onShow}>
+    <button className={`${button} ${isBump ? bump : undefined}`} onClick={onShow}>
       <span className={icon}>
         <CartIcon />
       </span>
