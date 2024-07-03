6c6b99ae2b960619bf3daf812d330bf852ffc4a0
jzinit <kyj32323@naver.com>
2024-07-03 10:46:13 +0900

    loader 기초 사용법

--- src/App.js
+++ src/App.js
@@ -5,21 +5,40 @@ import RootLayout from './components/RouteExample/layout/RootLayout';
 import ErrorPage from './components/RouteExample/pages/ErrorPage';
 import Events from './components/RouteExample/pages/Events';
 import EventDetail from './components/RouteExample/pages/EventDetail';
+import EventLayout from './components/RouteExample/layout/EventLayout';
 
 // 라우터 설정
 const router = createBrowserRouter([
-
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'events', element: <Events /> },
-      { path: 'events/:eventId', element: <EventDetail /> },
+      {
+        path: 'events',
+        element: <EventLayout />,
+        children: [
+          { 
+            index: true, 
+            element: <Events />,
+            // 이 페이지가 열릴때 자동으로 트리거되어 호출되는 함수
+            // 이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있습니다.
+            loader: async () => {
+              // console.log('loader call!!');
+              const response = await fetch('http://localhost:8282/events');
+              const jsonData = await response.json();
+
+              // loader가 리턴한 데이터는 loader를 선언한 컴포넌트와 그 하위 컴포넌트에서
+              // 언제든 불러서 사용할 수 있다.
+              return jsonData;
+            }
+          },
+          { path: ':eventId', element: <EventDetail /> },
+        ]
+      },
     ]
   },
-
 ]);
 
 const App = () => {
--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,32 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
-
+import React from 'react';
+import EventList from '../components/EventList';
+import { useLoaderData } from 'react-router-dom';
 
 const Events = () => {
 
-  const [eventList, setEventList] = useState([]);
-
-
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('http://localhost:8282/events');
-      const jsonData = await response.json();
-      setEventList(jsonData);
-    })();
-  ;
-  }, []);
+  // loader가 리턴한 데이터 받아오기
+  const eventList = useLoaderData();
+  //console.log(eventList);
 
   return (
     <>
       <h1>Events Page</h1>
-      <ul>
-        {eventList.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventList eventList={eventList} />
     </>
   );
 };
